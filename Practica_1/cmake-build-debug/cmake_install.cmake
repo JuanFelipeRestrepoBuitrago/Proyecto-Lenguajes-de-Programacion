5c2acb75934772586db29722066934eb30e4d7e3
Juan Felipe Restrepo Buitrago <jfeliperestrepob@gmail.com>
2022-09-07 18:25:01 -0500

    Proving a Bit

--- Practica_1/cmake-build-debug/cmake_install.cmake
+++ Practica_1/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: D:/Archivos de Aplicaciones/CLion/PROJECTS/Lenguajes de Programacion/ProyectoLenguajesProgramacion
+# Install script for directory: D:/Archivos de Aplicaciones/CLion/PROJECTS/Lenguajes de Programacion/Proyecto/Practica_1
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "D:/Archivos de Aplicaciones/CLion/PROJECTS/Lenguajes de Programacion/ProyectoLenguajesProgramacion/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "D:/Archivos de Aplicaciones/CLion/PROJECTS/Lenguajes de Programacion/Proyecto/Practica_1/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- Practica_1/main.cpp
+++ Practica_1/main.cpp
@@ -2,5 +2,6 @@
 #include "Node.h"
 int main() {
     Node *n = new Node("Juan", "1234", new Node("Pedro", "Dios lo bendiga"));
+    n -> add_user_at_the_end("Felipe", "1234");
     n -> print_list(n);
 }
